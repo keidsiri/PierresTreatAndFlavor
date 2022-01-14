1cb4e13759511a0d2ce55de3eeb2cb0028e12a48
Kate Kiatsiri <keidsiri@gmail.com>
2022-01-14 09:27:29 -0800

    update PierresTreatAndFlavorContext.cs

---
+++ PierresTreatAndFlavor/Models/PierresTreatAndFlavorContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PierresTreatAndFlavor.Models
+{
+  public class PierresTreatAndFlavorContext : DbContext
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<TreatFlavor> TreatFlavor { get; set; }
+
+    public PierresTreatAndFlavorContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
