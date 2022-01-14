9807c65592140cdce33a6527310bb81b8f2f8ae0
Kate Kiatsiri <keidsiri@gmail.com>
2022-01-14 12:26:00 -0800

    add AddFlavor and DeletFlavor functionality in TreatsController

--- PierresTreatAndFlavor/Controllers/TreatsController.cs
+++ PierresTreatAndFlavor/Controllers/TreatsController.cs
@@ -77,8 +77,31 @@ namespace PierresTreatAndFlavor.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult AddFlavor(int id)
+      {
+          var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+          ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+          return View(thisTreat);
+      }
 
+      [HttpPost]
+      public ActionResult AddFlavor(Treat treat, int FlavorId)
+      {
+          if (FlavorId != 0)
+          {
+          _db.TreatFlavor.Add(new TreatFlavor() { FlavorId = FlavorId, TreatId = treat.TreatId });
+          }
+          _db.SaveChanges();
+          return RedirectToAction("Index");
+      }
 
-
+      [HttpPost]
+      public ActionResult DeleteFlavor(int joinId)
+      {
+        var joinEntry = _db.TreatFlavor.FirstOrDefault(entry => entry.TreatFlavorId == joinId);
+        _db.TreatFlavor.Remove(joinEntry);
+        _db.SaveChanges();
+      return RedirectToAction("Index");
+      }
   }
 }
